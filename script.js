daa353fbed3138c8e366ec13f878feb1f36641d9
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-02-15 02:29:22 +0530

    Create script.js
    This is code for java script

---
+++ script.js
@@ -0,0 +1,25 @@
+let user  = prompt("Enter your choice");
+let cpuI = Math.floor(Math.random()*3);
+let cpu = ["S" , "W" , "G"];
+
+
+const game (user, cpu) {
+  if(cpu==user)
+    return "Choices matched"
+  else if(cpu=='S'&& user=='W')
+    return "cpu"
+  else if(cpu=='S'&& user=='G')
+    return "user"
+  else if(cpu=='W'&& user=='S')
+    return "cpu"
+  else if(cpu=='W' && user=="G")
+    return "cpu"
+  else if(cpu=='G' && user=="S")
+    return "You lose...hahahaha!!"
+  else if(cpu=='G' && user=="W")
+    return "user"
+}
+
+let result=game(cpu,user)
+document.write(`Your choice:${user} and Opponent choice:${cpu}`)
+document.write(`The Winner is ${result}`)   
